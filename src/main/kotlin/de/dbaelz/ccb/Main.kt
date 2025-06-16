65713626f591d6590eb09b78cfecd5931e0455ed
Daniel Bälz <info@dbaelz.de>
2025-06-16 20:25:19 +0200

    Use Clikt to parse the argument for the character description

--- src/main/kotlin/de/dbaelz/ccb/Main.kt
+++ src/main/kotlin/de/dbaelz/ccb/Main.kt
@@ -1,28 +1,30 @@
 package de.dbaelz.ccb
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.arguments.argument
 import de.dbaelz.ccb.builder.DEFAULT_CHARACTER_CARD_TEMPLATE
 import de.dbaelz.ccb.builder.SimpleCharacterCardBuilder
 import kotlinx.coroutines.runBlocking
-import kotlin.uuid.ExperimentalUuidApi
 
-@OptIn(ExperimentalUuidApi::class)
-fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("Usage: java -jar CharacterCardBuilder.jar <characterDescription>")
-        return
-    }
+fun main(args: Array<String>) = SimpleDescriptionCommand().main(args)
 
-    // Join all args in case user entered them with spaces instead of using quotes
-    val characterDescription = args.joinToString(" ")
+class SimpleDescriptionCommand : CliktCommand(name = "characterCardBuilder") {
+    private val characterDescription by argument(
+        name = "character description",
+        help = "The description of the character"
+    )
 
-    runBlocking {
-        println(
-            SimpleCharacterCardBuilder(
+    override fun run() {
+        runBlocking {
+            val result = SimpleCharacterCardBuilder(
                 model = MODEL,
                 template = DEFAULT_CHARACTER_CARD_TEMPLATE,
                 characterDescription = characterDescription
             ).run()
-        )
+
+            echo(result)
+        }
     }
 }
 
