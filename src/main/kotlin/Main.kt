ac9237c71307eedbaf05bcee748be32f67da9f53
Daniel Bälz <info@dbaelz.de>
2025-06-16 12:35:17 +0200

    Add first rudimentary implementation with hardcoded character name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,64 @@
 package de.dbaelz
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.features.eventHandler.feature.handleEvents
+import ai.koog.prompt.executor.llms.all.simpleOllamaAIExecutor
+import ai.koog.prompt.llm.LLMCapability
+import ai.koog.prompt.llm.LLMProvider
+import ai.koog.prompt.llm.LLModel
+import kotlinx.coroutines.runBlocking
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+@OptIn(ExperimentalUuidApi::class)
+fun main() {
+    val agent = AIAgent(
+        executor = simpleOllamaAIExecutor(),
+        systemPrompt = "You are an ai assistant to create character cards for role-play chat systems. " +
+                "Use the following template: $CHARACTER_CARD_TEMPLATE",
+        llmModel = LLModel(
+            provider = LLMProvider.Ollama,
+            id = "qwen3:8b",
+            capabilities = listOf(
+                LLMCapability.Temperature,
+                LLMCapability.Schema.JSON.Simple,
+                LLMCapability.Tools
+            )
+        )
+    ) {
+        handleEvents {
+            onAgentRunError { strategyName: String, sessionUuid: Uuid?, throwable: Throwable ->
+                println("An error occurred: ${throwable.message}\n${throwable.stackTraceToString()}")
+            }
+
+            onAgentFinished { strategyName: String, result: String? ->
+                println(result)
+            }
+        }
+    }
+
+    runBlocking {
+        agent.run("Create a character card for the historical character 'Ada Lovelace'")
     }
 }
+
+private const val CHARACTER_CARD_TEMPLATE = """
+character_card = [
+    "Name: ...",
+    "Surname: ...",
+    "Sex: ...",
+    "Age: ...",
+    "Nicknames: ...",
+    "Personality: ..., ..., ...",
+    "Body: ...",
+    "Likes: ..., ..., ...",
+    "Hates: ..., ..., ...",
+    "Clothes: ...",
+    "Species: ...",
+    "Race: ...",
+    "Occupation: ...",
+    "Appearance: ...",
+    "Description: ...",
+    "Attributes: ..., ..., ..."
+]
+"""
